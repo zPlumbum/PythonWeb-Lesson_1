69960ce6bf4695b2872ddb81bca1860da7e7e5cf
Alexander Kikot <academic77@yandex.ru>
2021-03-25 23:13:33 +0300

    Homework has been done.

--- api_requests.py
+++ api_requests.py
@@ -1,11 +1,6 @@
 import requests
 
 
-resp = requests.get('http://127.0.0.1:5000/health/')
-print(resp.status_code)
-print(resp.json())
-
-
 resp = requests.get('http://127.0.0.1:5000/users/8/')
 print(resp.status_code)
 print(resp.json())
@@ -17,6 +12,18 @@ resp = requests.post(
         'username': 'Vasya',
         'password': 'vasya12345',
         'email': 'vasya@yandex.ru'
+    }
+)
+print(resp.status_code)
+print(resp.json())
+
+
+resp = requests.post(
+    'http://127.0.0.1:5000/ads/',
+    json={
+        'title': 'sfasf',
+        'description': 'sfasdfascxv',
+        'user_id': 8
     }
 )
 print(resp.status_code)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import hashlib
+from datetime import datetime
 
 from flask import Flask, request, jsonify
 from flask.views import MethodView
@@ -20,7 +21,7 @@ class BasicException(Exception):
     status_code = 0
     default_message = 'Unknown Error'
 
-    def __init__(self, message: str = None, status_code: int = None):
+    def __init__(self, message=None, status_code=None):
         super().__init__(message)
         self.message = message
         request.status = self.status_code
@@ -75,12 +76,20 @@ class BaseModelMixin:
         except exc.IntegrityError:
             raise BadLuck
 
+    def delete(self):
+        db.session.delete(self)
+        try:
+            db.session.commit()
+        except exc.IntegrityError:
+            raise BadLuck
+
 
 class User(db.Model, BaseModelMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password = db.Column(db.String(128))
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    ads = db.relationship('Ad', backref='author', lazy=True)
 
     def __str__(self):
         return f'User {self.username}'
@@ -104,28 +113,77 @@ class User(db.Model, BaseModelMixin):
         }
 
 
+class Ad(db.Model, BaseModelMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.String(256), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __str__(self):
+        return f'Post {self.title}, created {self.created_at}'
+
+    def __repr__(self):
+        return str(self)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'user_id': self.user_id
+        }
+
+
 class UserView(MethodView):
 
-    def get(self, user_id):
+    @staticmethod
+    def get(user_id):
         user = User.by_id(user_id)
         return jsonify(user.to_dict())
 
-    def post(self):
+    @staticmethod
+    def post():
         user = User(**request.json)
         user.set_password(request.json['password'])
         user.add()
         return jsonify(user.to_dict())
 
-
-@app.route('/health/', methods=['GET', ])
-def health():
-    if request.method == 'GET':
-        return jsonify({'status': 'OK'})
-
-    return {'status': 'OK'}
+    @staticmethod
+    def delete(user_id):
+        user = User.by_id(user_id)
+        user.delete()
+        return jsonify({'response': 'User has been deleted'})
 
 
-app.add_url_rule('/users/<int:user_id>', view_func=UserView.as_view('users_get'), methods=['GET', ])
-app.add_url_rule('/users/', view_func=UserView.as_view('users_create'), methods=['POST', ])
+app.add_url_rule('/users/<int:user_id>', view_func=UserView.as_view('user_get'), methods=['GET', ])
+app.add_url_rule('/users/', view_func=UserView.as_view('user_create'), methods=['POST', ])
+app.add_url_rule('/users/<int:user_id>', view_func=UserView.as_view('user_delete'), methods=['DELETE', ])
+
+
+class AdView(MethodView):
+
+    @staticmethod
+    def get(ad_id):
+        ad = Ad.by_id(ad_id)
+        return jsonify(ad.to_dict())
+
+    @staticmethod
+    def post():
+        ad = Ad(**request.json)
+        ad.add()
+        return jsonify(ad.to_dict())
+
+    @staticmethod
+    def delete(ad_id):
+        ad = Ad.by_id(ad_id)
+        ad.delete()
+        return jsonify({'response': 'Ad has been deleted'})
+
+
+app.add_url_rule('/ad/<int:ad_id>', view_func=AdView.as_view('ad_get'), methods=['GET', ])
+app.add_url_rule('/ads/', view_func=AdView.as_view('ad_create'), methods=['POST', ])
+app.add_url_rule('/ad/<int:ad_id>', view_func=AdView.as_view('ad_delete'), methods=['DELETE', ])
+
+db.create_all(app=app)
 
 app.run()
